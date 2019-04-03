98941c0ed499565401089224754a30f9de75ea5d
titouan.robert <titouan.robert@datastorm.fr>
2019-04-03 13:58:49 +0200

    proj on dir & doc

---
+++ R/projectionOnDir.R
@@ -0,0 +1,32 @@
+#' Project on direction
+#' 
+#' 
+#' @param PT {data.table} Point for direction to project
+#' @param PTDF {data.frame | data.table}
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' 
+#'  CP = data.table()
+#'  CP$AT <- CP$DE <- CP$FR <- CP$NL <- 0
+#'  CP$BE <- 1
+#'  PROJ <- getProjPT(CP, PTDF)
+#' 
+#' }
+#'
+#' @export
+getProjPT <- function(PT, PTDF){
+  coefD <- PT/(max(abs(PT))/5)
+  CP2 <- coefD
+  k <- rep(0, nrow(PTDF))
+  while(all(k<PTDF$ram)){
+    PP <- PTDF[, .SD, .SDcols = grep(pattern = "ptdf", names(PTDF))]
+    for(i in 1:ncol(PP)){
+      k <- k + unlist(PP[, .SD, .SDcols = i]) * unlist(coefD[, .SD, .SDcols = i])
+    }
+    CP2 <- CP2 + coefD
+  }
+  CP2 <- CP2 - coefD
+  CP2
+}
